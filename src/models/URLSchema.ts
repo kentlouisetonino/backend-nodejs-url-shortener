1d17d375cd1f34f92ba5c4e4d29d2a767e48bb80
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-02-25 04:26:09 +0800

    move the interface above

--- src/models/URLSchema.ts
+++ src/models/URLSchema.ts
@@ -2,12 +2,12 @@ import mongoose, { Document } from 'mongoose'
 
 const Schema = mongoose.Schema
 
-export const URLSchema = new Schema<URLInterface>({
-  originalURL: { type: String, required: true },
-  shortURL: { type: Number, required: true },
-})
-
 export interface URLInterface extends Document {
   originalURL: string
   shortURL: number
 }
+
+export const URLSchema = new Schema<URLInterface>({
+  originalURL: { type: String, required: true },
+  shortURL: { type: Number, required: true },
+})
